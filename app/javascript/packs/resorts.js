6088e8b2234a30f75cdd744856b9d4fd693e0657
jeffreypwagner <wagnejef@mail.gvsu.edu>
2020-04-22 13:20:27 -0400

    fixed link url

--- app/javascript/packs/resorts.js
+++ app/javascript/packs/resorts.js
@@ -2,5 +2,5 @@ var resortSelect = document.getElementById("resort_id");
 
 resortSelect.onchange = function() {
     var resortNum = resortSelect.options[resortSelect.selectedIndex].value;
-    window.location.href = 'http://localhost:3000/resorts/' + resortNum;
+    window.location.href = 'https://ski-insider.herokuapp.com/resorts/' + resortNum;
 };
